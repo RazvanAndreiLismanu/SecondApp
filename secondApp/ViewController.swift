4b19eaf48abf5f9e0684a46a459d87bd79a899a4
Aula11 <aula11@imac-14.local>
2018-10-08 16:26:36 +0200

    A falta de view image

--- secondApp/ViewController.swift
+++ secondApp/ViewController.swift
@@ -8,10 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: properties
+    
+    @IBOutlet weak var nombreTxt: UITextField!
+    
+    @IBOutlet weak var nombreLbl: UILabel!
+    
+    
+    //MARK: actions
+    
+    @IBAction func setResetBtn(sender: UIButton) {
+        nombreLbl.text="Nombre ??"
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        nombreLbl.text = "Hola " + nombreTxt.text!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nombreTxt.delegate=self;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
