5da83630f231e58d69b72f81050d7dd4688018a8
Aula11 <aula11@imac-14.local>
2018-10-22 17:40:51 +0200

    Sesion03 hasta ejercicio2

--- secondApp/EvaluacionControl.swift
+++ secondApp/EvaluacionControl.swift
@@ -13,16 +13,18 @@ class EvaluacionControl: UIView {
     //MARK: atributos
     var gradoAfinidad = 0
     var botones = [UIButton]()
+    var tamaño = CGRect(x:0, y: 0, width: 44, height: 44)
     
 
     //MARK: inicialización
     required init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
-        for _ in 0..<5{
-            let boton = UIButton(frame: CGRect(x:0, y: 0, width: 44, height: 44))
+        for i in 0..<5{
+            let boton = UIButton(frame: tamaño)
             boton.backgroundColor = UIColor.redColor()
             boton.addTarget(self, action: #selector(btnEval(_:)), forControlEvents: .TouchDown)
             botones += [boton]
+            boton.tag=i
             addSubview(boton)
         }
         
@@ -34,12 +36,24 @@ class EvaluacionControl: UIView {
     
     override func layoutSubviews() {
         for (i, boton) in botones.enumerate() {
-            boton.frame.origin.x = CGFloat(i * (44 + 5)) }
+            boton.frame.origin.x = CGFloat(i * (44 + 5))
+        }
     }
     
     //MARK: actions
     func btnEval(boton: UIButton) {
-        print("Botón pulsado...👍")
+        switch boton.tag{
+        case 0:
+            print("Botón 1 pulsado...👍")
+        case 1:
+            print("Botón 2 pulsado...👍")
+        case 2:
+            print("Botón 3 pulsado...👍")
+        case 3:
+            print("Botón 4 pulsado...👍")
+        default:
+            print("Botón 5 pulsado...👍")
+        }
     }
     
     
