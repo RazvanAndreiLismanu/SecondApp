ebe165b40e13490e902ef87ef8733ac2d2dcf8d8
Aula11 <aula11@imac-14.local>
2018-10-22 17:13:18 +0200

    Sesion 03 hasta ejercicio 1

--- secondApp/EvaluacionControl.swift
+++ secondApp/EvaluacionControl.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class EvaluacionControl: UIView {
+    
+    //MARK: atributos
+    var gradoAfinidad = 0
+    var botones = [UIButton]()
+    
 
     //MARK: inicialización
     required init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
-        let boton = UIButton(frame: CGRect(x:0, y: 0, width: 44, height: 44))
-        boton.backgroundColor = UIColor.redColor()
-        addSubview(boton)
+        for _ in 0..<5{
+            let boton = UIButton(frame: CGRect(x:0, y: 0, width: 44, height: 44))
+            boton.backgroundColor = UIColor.redColor()
+            boton.addTarget(self, action: #selector(btnEval(_:)), forControlEvents: .TouchDown)
+            botones += [boton]
+            addSubview(boton)
+        }
+        
     }
     
     override func intrinsicContentSize() -> CGSize {
         return CGSize(width: 240, height: 44)
     }
+    
+    override func layoutSubviews() {
+        for (i, boton) in botones.enumerate() {
+            boton.frame.origin.x = CGFloat(i * (44 + 5)) }
+    }
+    
+    //MARK: actions
+    func btnEval(boton: UIButton) {
+        print("Botón pulsado...👍")
+    }
+    
     
 }
