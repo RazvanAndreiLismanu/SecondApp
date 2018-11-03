2c8039097bc0402a45f93ec5d4bc1c42bed32595
Razvan lismanu <razvanandreilismanu@Razvans-iMac.local>
2018-11-03 04:35:57 -0700

    Primer commit!

---
+++ secondApp/AmigoTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AmigoTableViewCell.swift
+//  secondApp
+//
+//  Created by Razvan andrei lismanu on 11/3/18.
+//  Copyright © 2018 Razvan andrei lismanu. All rights reserved.
+//
+
+import Cocoa
+
+class AmigoTableViewCell: UITableViewCell {
+
+}
